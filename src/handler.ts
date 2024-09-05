84c781b94d2647143787f7818b1ec29530807e14
RanjaniSakthivel <ranjani1296@gmail.com>
2024-09-05 13:22:21 +0530

    comments added

--- src/handler.ts
+++ src/handler.ts
@@ -4,6 +4,7 @@ import { APIGatewayEvent, Context, Callback } from 'aws-lambda';
 
 const server = awsServerlessExpress.createServer(app);
 
+//entrypoint
 export const handler = (event: APIGatewayEvent, context: Context, callback: Callback) => {
   awsServerlessExpress.proxy(server, event, context);
 };
